a02c26338f4589aa0d002cdedef77ecf002577bb
Axel <axaul@axaul.axaul>
2025-06-14 20:31:47 +0200

    Mise à jour de injection, login_screen et main

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,9 +1,11 @@
 import 'package:co_habit_frontend/config/constants/app_constants.dart';
 import 'package:co_habit_frontend/data/repositories/creer_foyer_repository_impl.dart';
+import 'package:co_habit_frontend/data/services/datasources/remote/auth_remote_datasource.dart';
 import 'package:co_habit_frontend/data/services/datasources/remote/foyer_remote_datasource.dart';
 import 'package:co_habit_frontend/domain/repositories/creer_foyer_repository.dart'; // Add this import
 import 'package:co_habit_frontend/domain/usecases/foyer/creer_foyer_usecase.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -21,6 +23,11 @@ void setUpDependencies() {
 
     return dio;
   });
+
+  // === Auth ===
+  getIt.registerLazySingleton<AuthRemoteDatasource>(
+      () => AuthRemoteDatasource(getIt<Dio>(),const FlutterSecureStorage()),
+  );
 
   // Data sources
   getIt.registerLazySingleton<FoyerRemoteDatasource>(
--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,23 +1,90 @@
+import 'package:co_habit_frontend/config/constants/app_constants.dart';
+import 'package:co_habit_frontend/core/di/injection.dart';
+import 'package:co_habit_frontend/domain/entities/user_credentials.dart';
+import 'package:co_habit_frontend/domain/usecases/auth/login_usecase.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+  
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController=TextEditingController();
+  bool _isLoading=false;
+  
+  Future<void> _handleLogin() async {
+    final loginUseCase = getIt<LoginUseCase>();
+    
+    final success=await loginUseCase(
+      UserCredentials(
+        email:_emailController.text.trim(),
+        password:_passwordController.text.trim(),
+      ),
+    );
+
+    setState(() => _isLoading = false);
+
+    if(success){
+      context.go('/choixInitial');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('La connexion a échoué')),
+      );
+    }
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('TEMP LOGIN SCREEN'),
-      ),
-      body: Center(
-        child: FilledButton(
-          onPressed: () {
-            context.go('/choixInitial');
-          },
-          child: const Text('Suivant'),
-        ),
+      appBar: AppBar(title:const Text('Connexion')),
+      body:Padding(
+        padding:const EdgeInsets.all(16.0),
+        child:Column(children:[
+          TextField(
+            controller:_emailController,
+            decoration:const InputDecoration(labelText:'Email'),
+          ),
+          TextField(
+            controller:_passwordController,
+            obscureText: true,
+            decoration:const InputDecoration(labelText:'Mot de passe'),
+          ),
+          const SizedBox(height:20),
+          _isLoading
+            ? const CircularProgressIndicator()
+            : ElevatedButton(
+              onPressed: _handleLogin,
+              child: const Text('Connexion'
+            ),
+          ),
+        ]),
       ),
     );
   }
 }
+
+// class LoginScreen extends StatelessWidget {
+//   const LoginScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('TEMP LOGIN SCREEN'),
+//       ),
+//       body: Center(
+//         child: FilledButton(
+//           onPressed: () {
+//             context.go('/choixInitial');
+//           },
+//           child: const Text('Suivant'),
+//         ),
+//       ),
+//     );
+//   }
+// }
