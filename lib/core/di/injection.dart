5fd5d66cfc848e96b69f7817f991ba048ae28f48
Axel GALLIC <axel.gallic@orange.fr>
2025-06-21 19:02:24 +0200

    ajout injection de dépendances

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,8 +1,11 @@
 import 'package:co_habit_frontend/config/constants/app_constants.dart';
+import 'package:co_habit_frontend/data/repositories/auth_repository_impl.dart';
 import 'package:co_habit_frontend/data/repositories/creer_foyer_repository_impl.dart';
 import 'package:co_habit_frontend/data/services/datasources/remote/auth_remote_datasource.dart';
 import 'package:co_habit_frontend/data/services/datasources/remote/foyer_remote_datasource.dart';
+import 'package:co_habit_frontend/domain/repositories/auth_repository.dart';
 import 'package:co_habit_frontend/domain/repositories/creer_foyer_repository.dart'; // Add this import
+import 'package:co_habit_frontend/domain/usecases/auth/login_usecase.dart';
 import 'package:co_habit_frontend/domain/usecases/foyer/creer_foyer_usecase.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -27,6 +30,15 @@ void setUpDependencies() {
   // === Auth ===
   getIt.registerLazySingleton<AuthRemoteDatasource>(
       () => AuthRemoteDatasource(getIt<Dio>(),const FlutterSecureStorage()),
+  );
+
+  // AuthRepository & LoginUseCase
+  getIt.registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImpl(getIt<AuthRemoteDatasource>()),
+  );
+
+  getIt.registerFactory<LoginUseCase>(
+      () => LoginUseCase(getIt<AuthRepository>()),
   );
 
   // Data sources
