474bd7608cc1b6b673a08496ab8b428a6989b985
cerensec <carlosceren2807@gmail.com>
2025-07-08 20:39:42 +0200

    modification login et signup

--- lib/presentation/screens/auth/login_screen.dart
+++ lib/presentation/screens/auth/login_screen.dart
@@ -1,12 +1,17 @@
+import 'package:co_habit_frontend/config/theme/app_theme.dart';
+import 'package:co_habit_frontend/presentation/screens/auth/widgets/social_login_button.dart';
+import 'package:co_habit_frontend/presentation/widgets/common_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:co_habit_frontend/core/di/injection.dart';
 import 'package:co_habit_frontend/data/models/models.dart';
 import 'package:co_habit_frontend/domain/usecases/usecases.dart';
 import 'package:co_habit_frontend/presentation/providers/auth_provider.dart';
 import 'package:co_habit_frontend/presentation/providers/foyer_provider.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   final String? initialMessage;
@@ -17,7 +22,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final _usernameController = TextEditingController();
+  final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   bool _isLoading = false;
 
@@ -28,25 +33,22 @@ class _LoginScreenState extends State<LoginScreen> {
     final foyerProvider = Provider.of<FoyerProvider>(context, listen: false);
 
     final success = await authProvider.login(
-      _usernameController.text.trim(),
+      _emailController.text.trim(),
       _passwordController.text.trim(),
     );
 
     setState(() => _isLoading = false);
-
     if (!mounted) return;
 
     if (success) {
       final prefs = await SharedPreferences.getInstance();
       final foyerId = prefs.getInt('foyerId');
-
       if (foyerId != null) {
         try {
           final foyer = await getIt<GetFoyerByIdUc>().execute(foyerId);
           foyerProvider.setFoyer(foyer as FoyerModel);
           if (mounted) context.go('/home');
-        } catch (e) {
-          // Si erreur (foyer supprimé, etc.), aller quand même à choixInitial
+        } catch (_) {
           if (mounted) context.go('/choixInitial');
         }
       } else {
@@ -74,34 +76,117 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Connexion')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(children: [
-          TextField(
-            controller: _usernameController,
-            decoration: const InputDecoration(labelText: 'Nom d\'utilisateur'),
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(labelText: 'Mot de passe'),
-          ),
-          const SizedBox(height: 20),
-          _isLoading
-              ? const CircularProgressIndicator()
-              : ElevatedButton(
-                  onPressed: _handleLogin,
-                  child: const Text('Connexion'),
+      backgroundColor: AppTheme.backgroundColor,
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: IntrinsicHeight(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          const SizedBox(height: 16),
+                          _buildTitle(),
+                          const SizedBox(height: 24),
+                          SizedBox(
+                              height: 180,
+                              child: Lottie.network(
+                                  'https://lottie.host/362be1e0-c892-46b7-aadf-f9c5bbb4b9a2/rYlurhfasK.json',
+                                  height: 290)),
+                          const SizedBox(height: 24),
+                          _buildLoginForm(),
+                          const SizedBox(height: 12),
+                          const Divider(height: 20, thickness: 1),
+                          const SizedBox(height: 10),
+                          const SocialLoginButton(
+                            icon: Icons.facebook_outlined,
+                            label: 'Continuer avec Facebook',
+                          ),
+                          const SizedBox(height: 12),
+                          const SocialLoginButton(
+                            icon: Icons.g_mobiledata_outlined,
+                            label: 'Continuer avec Google',
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          const SizedBox(height: 15),
+                          const Text(
+                            "Vous n'avez pas de compte ?",
+                            style: TextStyle(fontSize: 12, color: Colors.grey),
+                          ),
+                          TextButton(
+                            onPressed: () => context.push('/signup'),
+                            child: const Text(
+                              'Inscrivez-vous',
+                              style: TextStyle(color: Colors.blueAccent),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-          TextButton(
-            onPressed: () {
-              context.go('/signup');
-            },
-            child: const Text('Pas encore de compte ? Inscrivez-vous ici.'),
-          ),
-        ]),
+              ),
+            );
+          },
+        ),
       ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return const Text.rich(
+      TextSpan(
+        text: 'Bienvenue à ',
+        style: TextStyle(fontSize: 27),
+        children: [
+          TextSpan(
+              text: 'CoHabit', style: TextStyle(color: AppTheme.primaryColor)),
+        ],
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildLoginForm() {
+    return Column(
+      children: [
+        TextField(
+          controller: _emailController,
+          decoration: const InputDecoration(
+            labelText: 'Adresse e-mail',
+            prefixIcon: Icon(Icons.email_outlined),
+            border: OutlineInputBorder(),
+          ),
+          keyboardType: TextInputType.emailAddress,
+        ),
+        const SizedBox(height: 16),
+        TextField(
+          controller: _passwordController,
+          obscureText: true,
+          decoration: const InputDecoration(
+            labelText: 'Mot de passe',
+            prefixIcon: Icon(Icons.lock_outline),
+            border: OutlineInputBorder(),
+          ),
+        ),
+        const SizedBox(height: 20),
+        _isLoading
+            ? const CircularProgressIndicator()
+            : CohabitButton(
+                text: 'Connexion',
+                buttonType: ButtonType.gradient,
+                onPressed: _handleLogin,
+              ),
+      ],
     );
   }
 }
--- lib/presentation/screens/auth/signup_screen.dart
+++ lib/presentation/screens/auth/signup_screen.dart
@@ -2,7 +2,7 @@ import 'package:co_habit_frontend/core/di/injection.dart';
 import 'package:co_habit_frontend/core/services/validation_service.dart';
 import 'package:co_habit_frontend/domain/entities/register_data.dart';
 import 'package:co_habit_frontend/domain/usecases/auth/signup_usecase.dart';
-import 'package:co_habit_frontend/presentation/widgets/common/custom_form_field.dart';
+import 'package:co_habit_frontend/presentation/widgets/common_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
@@ -22,60 +22,17 @@ class _SignupScreenState extends State<SignupScreen> {
   final _phoneNumberController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-  bool _isLoading = false;
 
-  // Stockage des éléments sélectionnés dans le formulaire
   DateTime? _dateNaissance;
   String? _selectedGender;
-
-  Future<void> _handleSignup() async {
-    final signupUseCase = getIt<SignupUseCase>();
-    final String firstName = _firstNameController.text.trim();
-    final String lastName = _lastNameController.text.trim();
-    final String fullName = '$firstName $lastName';
-
-    final String? genderIsValid =
-        ValidationService.validateGender(_selectedGender);
-    if (genderIsValid != null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(genderIsValid)),
-      );
-      return;
-    }
-
-    final success = await signupUseCase(
-      RegisterData(
-        username: _usernameController.text.trim(),
-        fullName: fullName,
-        firstName: firstName,
-        lastName: lastName,
-        // La date est stockée au format dd/MM/yyyy pour l'affichage user, mais l'api le veut au format yyyy-MM-dd donc on doit la formatter ici.
-        birthDate: DateFormat('yyyy-MM-dd').format(_dateNaissance!),
-        gender:
-            _selectedGender!, // Si on arrive ici c'est que le selectedGender n'est pas null !
-        phoneNumber: _phoneNumberController.text.trim(),
-        email: _emailController.text.trim(),
-        password: _passwordController.text.trim(),
-      ),
-    );
-
-    setState(() => _isLoading = false);
-
-    if (mounted) {
-      if (success) {
-        context.go('/login',
-            extra: 'Inscription réussie, vous pouvez vous connecter !');
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('L\'inscription a échoué...')),
-        );
-      }
-    }
-  }
+  bool _isLoading = false;
 
   Future<void> _selectDate() async {
     final DateTime? picked = await showDatePicker(
-        context: context, firstDate: DateTime(1900), lastDate: DateTime.now());
+      context: context,
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
 
     if (picked != null && picked != _dateNaissance) {
       setState(() {
@@ -85,88 +42,151 @@ class _SignupScreenState extends State<SignupScreen> {
     }
   }
 
+  Future<void> _handleSignup() async {
+    final signupUseCase = getIt<SignupUseCase>();
+    final firstName = _firstNameController.text.trim();
+    final lastName = _lastNameController.text.trim();
+
+    final genderValidation = ValidationService.validateGender(_selectedGender);
+    if (genderValidation != null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(genderValidation)),
+      );
+      return;
+    }
+
+    setState(() => _isLoading = true);
+
+    final success = await signupUseCase(
+      RegisterData(
+        username: _usernameController.text.trim(),
+        fullName: '$firstName $lastName',
+        firstName: firstName,
+        lastName: lastName,
+        birthDate: DateFormat('yyyy-MM-dd').format(_dateNaissance!),
+        gender: _selectedGender!,
+        phoneNumber: _phoneNumberController.text.trim(),
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      ),
+    );
+
+    setState(() => _isLoading = false);
+
+    if (!mounted) return;
+
+    if (success) {
+      context.go('/login',
+          extra: 'Inscription réussie, vous pouvez vous connecter !');
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('L\'inscription a échoué...')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Inscription')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(children: [
-          CustomFormField(
-            controller: _usernameController,
-            label: 'Nom d\'utilisateur',
-            validator: (value) =>
-                ValidationService.validateName('username', value),
-            inputType: TextInputType.text,
+      appBar: const ScreenAppBar(title: 'Inscription'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _buildForm(),
+                const SizedBox(height: 30),
+                _isLoading
+                    ? const Center(child: CircularProgressIndicator())
+                    : CohabitButton(
+                        text: 'S\'inscrire',
+                        buttonType: ButtonType.gradient,
+                        onPressed: _handleSignup,
+                      ),
+              ],
+            ),
           ),
-          CustomFormField(
-              controller: _firstNameController,
-              label: 'Prénom',
-              validator: (value) =>
-                  ValidationService.validateName('prénom', value),
-              inputType: TextInputType.text),
-          CustomFormField(
-            controller: _lastNameController,
-            label: 'Nom',
-            validator: (value) =>
-                ValidationService.validateName('nom de famille', value),
-            inputType: TextInputType.text,
-          ),
-          CustomFormField(
-            controller: _birthDateController,
-            label: 'Date de naissance',
-            hintText: '01/01/1970',
-            readOnly: true,
-            suffixIcon: IconButton(
-                onPressed: _selectDate, icon: const Icon(Icons.calendar_today)),
-            validator: (_) =>
-                ValidationService.validateBirthDate(_dateNaissance),
-          ),
-          DropdownButtonFormField<String>(
-            value: _selectedGender,
-            items: const [
-              DropdownMenuItem(value: 'Homme', child: Text('Homme')),
-              DropdownMenuItem(value: 'Femme', child: Text('Femme')),
-            ],
-            onChanged: (value) {
-              setState(() {
-                _selectedGender = value;
-              });
-            },
-            decoration: const InputDecoration(labelText: 'Sexe'),
-          ),
-          CustomFormField(
-            controller: _phoneNumberController,
-            label: 'Numéro de téléphone',
-            maxLength: 10,
-            validator: (value) => ValidationService.validatePhoneNumber(value),
-            inputType: TextInputType.number,
-          ),
-          CustomFormField(
-            controller: _emailController,
-            label: 'Adresse e-mail',
-            inputType: TextInputType.emailAddress,
-          ),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(labelText: 'Mot de passe'),
-          ),
-          const SizedBox(height: 20),
-          _isLoading
-              ? const CircularProgressIndicator()
-              : ElevatedButton(
-                  onPressed: _handleSignup,
-                  child: const Text(
-                    'S\'inscrire',
-                  ),
-                ),
-          TextButton(
-              onPressed: () => context.go('/login'),
-              child: const Text(
-                  'Vous avez déjà un compte ? Connectez-vous ici !')),
-        ]),
+        ),
       ),
+    );
+  }
+
+  Widget _buildForm() {
+    return Column(
+      children: [
+        CustomFormField(
+          controller: _usernameController,
+          label: 'Nom d\'utilisateur',
+          validator: (value) =>
+              ValidationService.validateName('username', value),
+        ),
+        const SizedBox(height: 16),
+        CustomFormField(
+          controller: _firstNameController,
+          label: 'Prénom',
+          validator: (value) => ValidationService.validateName('prénom', value),
+        ),
+        const SizedBox(height: 16),
+        CustomFormField(
+          controller: _lastNameController,
+          label: 'Nom',
+          validator: (value) =>
+              ValidationService.validateName('nom de famille', value),
+        ),
+        const SizedBox(height: 16),
+        CustomFormField(
+          controller: _birthDateController,
+          label: 'Date de naissance',
+          hintText: '01/01/1970',
+          readOnly: true,
+          suffixIcon: IconButton(
+            onPressed: _selectDate,
+            icon: const Icon(Icons.calendar_today),
+          ),
+          validator: (_) => ValidationService.validateBirthDate(_dateNaissance),
+        ),
+        const SizedBox(height: 16),
+        DropdownButtonFormField<String>(
+          value: _selectedGender,
+          decoration: const InputDecoration(
+            labelText: 'Sexe',
+            border: OutlineInputBorder(),
+          ),
+          items: const [
+            DropdownMenuItem(value: 'Homme', child: Text('Homme')),
+            DropdownMenuItem(value: 'Femme', child: Text('Femme')),
+            DropdownMenuItem(
+                value: 'Autre', child: Text('Autre / Je préfère ne pas dire')),
+          ],
+          onChanged: (value) => setState(() => _selectedGender = value),
+        ),
+        const SizedBox(height: 16),
+        CustomFormField(
+          controller: _phoneNumberController,
+          label: 'Numéro de téléphone',
+          maxLength: 10,
+          inputType: TextInputType.number,
+          validator: (value) => ValidationService.validatePhoneNumber(value),
+        ),
+        const SizedBox(height: 16),
+        CustomFormField(
+          controller: _emailController,
+          label: 'Adresse e-mail',
+          inputType: TextInputType.emailAddress,
+        ),
+        const SizedBox(height: 16),
+        TextField(
+          controller: _passwordController,
+          obscureText: true,
+          decoration: const InputDecoration(
+            labelText: 'Mot de passe',
+            prefixIcon: Icon(Icons.lock_outline),
+            border: OutlineInputBorder(),
+          ),
+        ),
+      ],
     );
   }
 }
